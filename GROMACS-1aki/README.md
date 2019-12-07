911909157eecf10374ae1434559bb70f07d865cc
Andrew Garcia <garcia.gtr@gmail.com>
2019-12-07 12:11:38 -0500

    add files

---
+++ GROMACS-1aki/README.md
@@ -0,0 +1,5 @@
+# Gromacs Lysozyme Tutorial [Generated Files from]
+### Andrew Garcia
+
+Tutorial link:
+http://www.mdtutorials.com/gmx/lysozyme/index.html
---
+++ GROMACS-1aki/plotmaking.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 11:08:18 2019
+
+@author: garci
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import xlwings as xw
+import pandas
+import os
+
+import sys
+sys.path.append('C:/Users/garci/Dropbox (Personal)/scripts/python_import/')
+from process_output import readxl
+
+path = r'C:\Users\garci\Dropbox (Personal)\scripts\GROMACS-learning\GROMACS-1aki/'
+file = 'data.xlsx'
+readxl(path,file,sheet='Sheet1')
+readxl(path,file,sheet='Sheet2')
+readxl(path,file,sheet='Sheet3')
+readxl(path,file,sheet='Sheet4')
+readxl(path,file,sheet='Sheet5')
